9100f7c4108dfabd6a3656976df01e704dcc52e8
Chendev <panhaseth.sophat@outlook.com>
2026-03-10 23:17:43 +0700

    Finish Exercise 4

--- exercise-4/src/components/Carousel.jsx
+++ exercise-4/src/components/Carousel.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 export const Carousel = ({ images }) => {
-  /* You will need to  use  state to mnage the current image */
+  // State: we need 1 state to track the current image index
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  /* You will need to hanle the click on left and right button */
+  // Handle left arrow click
+  function handleLeftClick() {
+    // If we're on the first image (index 0), go to the last image
+    // Otherwise, go to the previous image
+    setCurrentIndex((prevIndex) => 
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  }
 
-  /* You will need to manage the cases when we are on the last image or first image*/
+  // Handle right arrow click
+  function handleRightClick() {
+    // If we're on the last image, go to the first image (index 0)
+    // Otherwise, go to the next image
+    setCurrentIndex((prevIndex) => 
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  }
 
   return (
     <div className="carousel">
-      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      <BsArrowLeftCircleFill 
+        className="arrow arrow-left" 
+        onClick={handleLeftClick}
+      />
 
-      {/* YOu will need to display the current image, not the first one.. */}
-      <img src={images[0].src} alt={images[0].alt} className="slide" />
+      {/* Display the current image based on currentIndex */}
+      <img 
+        src={images[currentIndex].src} 
+        alt={images[currentIndex].alt} 
+        className="slide" 
+      />
 
-      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <BsArrowRightCircleFill 
+        className="arrow arrow-right" 
+        onClick={handleRightClick}
+      />
     </div>
   );
 };
